470a561f205230cd83deae2a4f44647c762e059c
강현수 <khsofficial1213@gmail.com>
2024-03-25 22:45:47 +0900

    feat: Chapter 4, 5 정리중

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import DataBinding from '../views/DataBinding.vue';
+import DataBindingHtml from '../views/DataBindingHtml.vue';
+import DataBindingSelect from '../views/DataBindingSelect.vue';
+import DataBindingCheckbox from '../views/DataBindingCheckbox.vue';
+import DataBindingRadio from '../views/DataBindingRadio.vue';
 
 const routes = [
   {
@@ -15,6 +20,31 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+  },
+  {
+    path: '/databinding',
+    name: 'DataBinding',
+    component: DataBinding,
+  },
+  {
+    path: '/databindinghtml',
+    name: 'DataBindingHtml',
+    component: DataBindingHtml,
+  },
+  {
+    path: '/databindingselect',
+    name: 'DataBindingSelect',
+    component: DataBindingSelect,
+  },
+  {
+    path: '/databindingcheck',
+    name: 'DataBindingCheckbox',
+    component: DataBindingCheckbox,
+  },
+  {
+    path: '/databindingradio',
+    name: 'DataBindingRadio',
+    component: DataBindingRadio,
   },
 ];
 
---
+++ src/views/DataBindingHtml.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>{{ htmlString }}</div>
+    <div v-html="htmlString"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      htmlString: '<p style="color:red;">This is a read String.</p>',
+    };
+  },
+};
+</script>
+
+<style></style>
